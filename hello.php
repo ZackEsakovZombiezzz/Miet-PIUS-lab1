fbe5273b458e198d586a00546bf1cb7e9b4152e2
ZackEsakovZombiezzz <zaharesakov@gmail.com>
2023-02-14 15:10:07 +0300

    New branch and add gitignore

--- hello.php
+++ hello.php
@@ -1,2 +1,22 @@
 <?php 
- echo "Hello from php";
+ require_once __DIR__.'/vendor/autoload.php';
+ use Symfony\Component\Validator\Constraints\Length;
+ use Symfony\Component\Validator\Constraints\NotBlank;
+ use Symfony\Component\Validator\Validation;
+
+ $validator = Validation::createValidator();
+ $name = $_POST['title'];
+ $violations = $validator->validate($name, [
+     new Length(['min' => 10]),
+     new NotBlank(),
+ ]);
+ 
+ if (0 !== count($violations)) {
+     // there are errors, now you can show them
+     foreach ($violations as $violation) {
+         echo $violation->getMessage().'<br>';
+         echo $violation;
+     }
+ }else{
+    echo "hi";
+ }
